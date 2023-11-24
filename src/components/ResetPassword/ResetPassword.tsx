3b3248febff1f99ade0a709e644bba2e159230fd
AnetaPy <aneta.pych15@gmail.com>
2023-11-24 20:00:07 +0100

    Adding the ResetPassword component to the common directory.

---
+++ src/components/ResetPassword/ResetPassword.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {Input} from "../common/Input/Input";
+import {Btn} from "../common/Btn/Btn";
+import {AuthorizationInfo} from "../common/AuthorizationInfo/AuthorizationInfo";
+import './ResetPassword.css';
+
+export const ResetPassword = () => {
+    return (
+        <div className="authorization_container">
+            <div className="authorization_wrapper">
+                <img src="/assets/megak.png" alt="MegaK logo"/>
+                <form>
+                    <Input type="text" placeholder="E-mail"/>
+                    <Btn text="Wyślij wiadomość"></Btn>
+                </form>
+                <AuthorizationInfo text="Wysłano e-mail. Sprawdz skrzynkę odbiorczą. - !!Do zrobiania!!"/>
+            </div>
+        </div>
+    )
+}
