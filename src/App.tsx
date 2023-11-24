94793c8d57489048b8789d83bc9444f295170415
AnetaPy <aneta.pych15@gmail.com>
2023-11-24 16:02:03 +0100

    Creating routing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
-import './App.css'
 import React from "react";
+import {LogIn} from "./components/LogIn/LogIn";
+import {Route, Routes} from "react-router-dom";
+import './App.css'
 
 function App() {
 
     return (
-            <div className="authorization_container">
-                <div className="authorization_wrapper">
-                    <img src="/assets/megak.png" alt="MegaK logo"/>
-                    <form>
-                        <input type="text" placeholder="E-mail"/>
-                        <input type="password" placeholder="Hasło"/>
-                        <a href="#">Zapomniałeś hasła?</a>
-                        <div className="go_to_registration">
-                        <p>Nie masz konta?</p><a href="#">Zarejestruj się</a>
-                        <button>Zaloguj się</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
+        <>
+            <Routes>
+                <Route path="/" element={<LogIn/>}/>
+            </Routes>
+        </>
     )
 }
 
