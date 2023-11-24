ff2b0183ebd5b5f584eb53be208a8e208026d743
AnetaPy <aneta.pych15@gmail.com>
2023-11-24 14:51:20 +0100

    Creating basic authorization view structures.

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,20 @@ import React from "react";
 function App() {
 
     return (
-        <>
-
-        </>
+            <div className="authorization_container">
+                <div className="authorization_wrapper">
+                    <img src="/assets/megak.png" alt="MegaK logo"/>
+                    <form>
+                        <input type="text" placeholder="E-mail"/>
+                        <input type="password" placeholder="Hasło"/>
+                        <a href="#">Zapomniałeś hasła?</a>
+                        <div className="go_to_registration">
+                        <p>Nie masz konta?</p><a href="#">Zarejestruj się</a>
+                        <button>Zaloguj się</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
     )
 }
 
