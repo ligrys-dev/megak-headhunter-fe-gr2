ae64605eb9c6b74a5416320cd144ede94047fe32
AnetaPy <aneta.pych15@gmail.com>
2023-11-25 18:46:53 +0100

    A form handler has been added for the LogIn component.

--- src/components/LogIn/LogIn.tsx
+++ src/components/LogIn/LogIn.tsx
@@ -1,43 +1,51 @@
-import React, {FormEvent, useRef, useState} from "react";
+import {FormEvent, useState} from "react";
 import {Btn} from "../common/Btn/Btn";
 import {redirect} from "react-router-dom";
 import './LogIn.css';
 
 export const LogIn = () => {
     const [loading, setLoading] = useState(false);
-    const email = useRef("");
-    const password = useRef("");
+    const [form, setForm] = useState({
+        email: '',
+        password: '',
+    });
+
+    const updateForm = async (key: string, value: any) => {
+        setForm(form => ({
+            ...form,
+            [key]: value
+        }))
+    };
+
     const onSubmit = async (e: FormEvent) => {
         e.preventDefault();
-        console.log('poszło')
-        // const res = await fetch(`http://localhost:3001/user/login`, {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //     },
-        //     body: JSON.stringify({
-        //         email,
-        //         password
-        //     })
-        // });
-        // const data = await res.json();
-        // if (data) {
-        //     setLoading(true)
-        // } else {
-        //     alert("Błędny login lub hasło!");
-        // }
+        const res = await fetch(`http://localhost:3306/user/login`, {
+            // @TODO Return to the url
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(form)
+        });
+        const data = await res.json();
+        if (data) {
+            setLoading(true)
+        } else {
+            alert("Błędny login lub hasło!");
+        }
     }
 
     if (loading){
         redirect('/home')
     }
+
     return (
         <div className="authorization_container">
             <div className="authorization_wrapper">
                 <img src="/assets/megak.png" alt="MegaK logo"/>
                 <form onSubmit={onSubmit}>
-                    <input type="text" placeholder="E-mail" onChange={(e) => (email.current = e.target.value)}/>
-                    <input type="password" placeholder="Hasło" onChange={(e) => (email.current = e.target.value)}/>
+                    <input type="email" placeholder="E-mail" onChange={e => updateForm('email', e.target.value)}/>
+                    <input type="password" placeholder="Hasło" onChange={e => updateForm('password', e.target.value)}/>
                     <a href="/reset-password">Zapomniałeś hasła?</a>
                     <div className="go_to_registration">
                         <p>Nie masz konta?</p><a href="/signin">Zarejestruj się</a>
