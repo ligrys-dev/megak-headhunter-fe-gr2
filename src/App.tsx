70523d142c63df39f8e4a4c62aa13dd377bf1484
AnetaPy <aneta.pych15@gmail.com>
2023-11-29 13:10:16 +0100

    Added routing to the StudentPanel component.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {SignUp} from "./components/SignUp/SignUp";
 import {ResetPassword} from "./components/ResetPassword/ResetPassword";
 import {AdminPanel} from "./components/AdminPanel/AdminPanel.tsx";
 import './App.css';
+import {StudentPanel} from "./components/StudentPanel/StudentPanel";
 
 function App() {
     return (
@@ -14,6 +15,7 @@ function App() {
                 <Route path="/signup" element={<SignUp/>}/>
                 <Route path="/reset-password" element={<ResetPassword/>}/>
                 <Route path="/admin" element={<AdminPanel/>}/>
+                <Route path="/student" element={<StudentPanel/>}/>
             </Routes>
         </>
     )
