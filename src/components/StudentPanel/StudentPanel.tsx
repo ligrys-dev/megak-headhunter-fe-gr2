30ddcb152e724e3f6d73875b995ea08602c4527e
AnetaPy <aneta.pych15@gmail.com>
2023-11-29 20:32:16 +0100

    Added user

--- src/components/StudentPanel/StudentPanel.tsx
+++ src/components/StudentPanel/StudentPanel.tsx
@@ -1,18 +1,34 @@
 import {useState} from 'react';
 import {PanelHeader} from "../common/PanelHeader/PanelHeader";
 import './StudentPanel.css';
+import {ContractType, NewStudentEntity, TypeWork} from "types";
 
 export const StudentPanel = () => {
-    const [user, setuser] = useState({
-        name: 'Student',
+    const [user, setUser] =useState<NewStudentEntity>({
+        email: 'acb@abc.pl',
+        tel: 123456789,
+        firstName: 'Student',
         lastName: 'Testowy',
-        userGitHub: ''
+        avatar: null,
+        githubUsername: '',
+        portfolioUrls: [''],
+        projectUrls: [''],
+        bio: '',
+        expectedTypeWork: TypeWork.DoesNotMatter,
+        targetWorkCity: '',
+        expectedContractType: ContractType.CONTRACT,
+        expectedSalary: null,
+        canTakeApprenticeship: false,
+        monthsOfCommercialExp: 0,
+        education: '',
+        workExperience: null,
+        courses: null
     })
 
     return (
         <>
             <div className="student_panel">
-                <PanelHeader name={user.name} lastName={user.lastName}  urlAccount="/student1" urlLogOut="/logout1" userGitHub={user.userGitHub}/>
+                <PanelHeader name={user.firstName} lastName={user.lastName}  urlAccount="/student1" urlLogOut="/logout1" githubUsername={user.githubUsername}/>
                 <div className="panel_main">
                 </div>
             </div>
