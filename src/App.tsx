cb879e36f40dc29fd4fa92a447afc6960d1c5542
AnetaPy <aneta.pych15@gmail.com>
2023-12-04 15:31:52 +0100

    Removing the user registration view

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import {LogIn} from "./components/LogIn/LogIn";
 import {Route, Routes} from "react-router-dom";
-import {SignUp} from "./components/SignUp/SignUp";
 import {ResetPassword} from "./components/ResetPassword/ResetPassword";
 import {AdminPanel} from "./components/AdminPanel/AdminPanel.tsx";
 import {StudentPanel} from "./components/StudentPanel/StudentPanel";
@@ -11,7 +10,6 @@ function App() {
         <div className="app-container">
             <Routes>
                 <Route path="/" element={<LogIn/>}/>
-                <Route path="/signup" element={<SignUp/>}/>
                 <Route path="/reset-password" element={<ResetPassword/>}/>
                 <Route path="/admin" element={<AdminPanel/>}/>
                 <Route path="/student" element={<StudentPanel/>}/>
