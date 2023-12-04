475848f095824a3900e42923d286101c51934b45
AnetaPy <aneta.pych15@gmail.com>
2023-12-04 13:12:10 +0100

    Added StudentExperience component in Education and Experience section

---
+++ src/components/common/StudentEducationAndExperience/StudentExperience/StudentExperience.tsx
@@ -0,0 +1,41 @@
+import {StudentProfileInterface} from "types";
+import './StudentExperience.css';
+
+interface Props {
+    user: StudentProfileInterface
+}
+
+export const StudentExperience = (props: Props) => {
+    const {workExperience, portfolioUrls, projectUrls, initialData} = props.user;
+
+    const setPortfolioLinks = (portfolioUrls) => {
+        return (
+            portfolioUrls.map((url, index) => (
+                    <div key={index} className="portfolio-links">
+                        <img src="/assets/clip.png" alt="Clip image"/>
+                        <p>{url}</p>
+                    </div>
+                )
+            )
+        )
+    }
+
+    return (
+        <div className="student-experience">
+            <h2>Doświadczenie zawodowe</h2>
+            <p>{workExperience ? workExperience : 'Brak danych'}</p>
+            <h2>Portfolio</h2>
+            <div className="links-container">
+                {portfolioUrls ? setPortfolioLinks(portfolioUrls) : 'Brak danych'}
+            </div>
+            <h2>Projekt w zespole Scrumowym</h2>
+            <div className="links-container">
+                {initialData.bonusProjectUrls ? setPortfolioLinks(initialData.bonusProjectUrls) : 'Brak danych'}
+            </div>
+            <h2>Projekt na zaliczenie</h2>
+            <div className="links-container">
+                {projectUrls ? setPortfolioLinks(projectUrls) : 'Brak danych'}
+            </div>
+        </div>
+    )
+}
