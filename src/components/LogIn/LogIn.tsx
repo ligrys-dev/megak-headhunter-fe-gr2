73578f26e9c74afe719907386e9cf4416f15cac2
AnetaPy <aneta.pych15@gmail.com>
2023-12-05 20:23:57 +0100

    Login updated

--- src/components/LogIn/LogIn.tsx
+++ src/components/LogIn/LogIn.tsx
@@ -1,16 +1,21 @@
 import {FormEvent, useState} from "react";
 import {Btn} from "../common/Btn/Btn";
-import {redirect} from "react-router-dom";
-import {Spinner} from "../common/Spinner/Spinner";
 import './LogIn.css';
 
+
 interface formType {
-    email:string;
+    email: string;
     password: string;
 }
 
+interface dataType {
+    id: string;
+    role: number;
+}
+
 export const LogIn = () => {
-    const [loading, setLoading] = useState<boolean | null>(null);
+    const [loading, setLoading] = useState(false);
+    const [response, setResponse] = useState<dataType | null>(null)
     const [form, setForm] = useState<formType>({
         email: '',
         password: '',
@@ -25,30 +30,33 @@ export const LogIn = () => {
 
     const onSubmit = async (e: FormEvent) => {
         e.preventDefault();
-        setLoading(true);
-        try {
-            const res = await fetch(`http://localhost:3001/login`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(form)
-            });
-            if (res.status === 400 || res.status === 500) {
-                alert('Błędny login lub hasło.')
-                return;
-            }
-        } finally {
-            setLoading(false);
+        const res = await fetch('http://localhost:3001/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(form)
+        });
+        const data = await res.json();
+        console.log(data)
+        if (res.status === 400 || res.status === 401 || res.status === 404 || res.status === 500) {
+            alert('Błędny login lub hasło.');
+            return;
+        } else {
+            setResponse(data);
+            setLoading(true);
         }
     }
 
     if (loading) {
-        return <Spinner/>
-    }
-
-    if (!loading) {
-        redirect("http://localhost:5173/")
+        console.log(response?.role)
+        if (response?.role === 1) {
+            console.log('admin')
+        } else if (response?.role === 2) {
+            console.log('student')
+        } else if (response?.role === 2) {
+            console.log('hr')
+        }
     }
 
     return (
