7121ac18514a3532dfa7db979397c3ec4163efb6
AnetaPy <aneta.pych15@gmail.com>
2023-12-07 14:17:31 +0100

    Added proper URL to return from the No Access component

--- src/components/common/NoAccess/NoAccess.tsx
+++ src/components/common/NoAccess/NoAccess.tsx
@@ -1,10 +1,23 @@
 import './NoAccess.css';
 
-export const NoAccess = () => {
+interface Props {
+    role: number;
+}
+
+export const NoAccess = (props:Props) => {
+    let url;
+    if (props.role === 1) {
+        url = "/admin"
+    } else if (props.role === 2) {
+        url = "/student"
+    } else if (props.role === 3) {
+        url = "/hr"
+    }
+
     return (
         <div className="no-access-container">
             <h2>Nie masz dostępu!</h2>
-            <a href="/">Wróć do strony głównej</a>
+            <a href={url}>Wróć do strony głównej</a>
         </div>
     )
 }
