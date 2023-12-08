3553640ae27072394712ea7769a67f7eb0754a61
AnetaPy <aneta.pych15@gmail.com>
2023-12-08 12:56:58 +0100

    Added support for password reset form.

--- src/components/ResetPassword/ResetPassword.tsx
+++ src/components/ResetPassword/ResetPassword.tsx
@@ -1,41 +1,42 @@
 import {Btn} from "../common/Btn/Btn";
 import {AuthorizationInfo} from "../common/AuthorizationInfo/AuthorizationInfo";
 import {FormEvent, useRef, useState} from "react";
-import {Spinner} from "../common/Spinner/Spinner";
 import './ResetPassword.css';
 
 export const ResetPassword = () => {
-    const [loading, setLoading] = useState<boolean | null>(null);
-    // @TODO Add type for form
-    const [email, setEmail] = useState<string>('');
-    const text = useRef('')
+    const [response, setResponse] = useState<boolean | null>(null);
+    const [email, setEmail] = useState<string>({
+        email: ''
+    });
+    const text = useRef('');
+
+    const updateForm = async (key: string, value: any) => {
+        setEmail(email => ({
+            ...email,
+            [key]: value
+        }))
+    };
 
     const onSubmit = async (e: FormEvent) => {
         e.preventDefault();
-        setLoading(true);
-        try {
-            const res = await fetch(`http://localhost:3306/user/login`, {
-                // @TODO Return to the url
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(email)
-            });
-            if (res.status === 400 || res.status === 500) {
-                alert(`Nie ma ${email} w bazie.`)
-                return;
-            }
-        } finally {
-            setLoading(false);
+        const res = await fetch('http://localhost:3001/user/reset-pass', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(email)
+        });
+        const data = await res.json();
+
+        if (res.status === 400 || res.status === 401 || res.status === 404 || res.status === 500) {
+            alert('Błędny e-mail. Wpisz jeszcze raz.');
+            return;
+        } else {
+            setResponse(data.ok);
         }
     }
 
-    if (loading) {
-        return <Spinner/>
-    }
-
-    if (loading === false) {
+    if (response) {
         text.current = 'Wysłano wiadomość. Sprawdź e-mail.'
     }
 
@@ -44,7 +45,7 @@ export const ResetPassword = () => {
             <div className="authorization_wrapper">
                 <img src="/assets/megak.png" alt="MegaK logo"/>
                 <form className="reset_password" onSubmit={onSubmit}>
-                    <input type="text" placeholder="E-mail" onChange={e => setEmail(e.target.value)}/>
+                    <input type="email" placeholder="E-mail" onChange={e => updateForm('email', e.target.value)}/>
                     <Btn text="Wyślij wiadomość"></Btn>
                 </form>
                 <AuthorizationInfo text={text.current}/>
