a44f7d39bb141c8780978648d836798cf09a6d55
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2023-12-11 19:33:35 +0100

    editpersonaldata html elements converted into inputs

--- src/components/common/EditStudent/EditStudentPersonalData/EditStudentPersonalData.tsx
+++ src/components/common/EditStudent/EditStudentPersonalData/EditStudentPersonalData.tsx
@@ -10,10 +10,9 @@ interface Props {
 
 export const EditStudentPersonalData = (props: Props) => {
     const {form} = useContext(EditStudentDataContext)
-    const {tel, githubUsername, firstName, initialData, bio, lastName, status} = props.user;
 
     const checkStatus = () => {
-        switch (status) {
+        switch (form.status) {
             case 0:
                 return 'Dostępny';
             case 1:
@@ -29,27 +28,27 @@ export const EditStudentPersonalData = (props: Props) => {
         <div className="student-personal-info">
             <div className="student-personal-info-left-side">
                 <img id="user-image"
-                     src={githubUsername ? `https://github.com/${githubUsername}.png` : "/assets/user.png"}
+                     src={form.githubUsername ? `https://github.com/${form.githubUsername}.png` : "/assets/user.png"}
                      alt=""/>
                 <input value={form.firstName} onChange={e => props.onChange('firstName', e.target.value)}></input> 
                 <input value={form.lastName} onChange={e => props.onChange('lastName', e.target.value)}></input>
                 <div className="github-info">
                     <img src="/assets/github.png" alt="GitHub logo"/>
-                    <p>{githubUsername ? githubUsername : 'Brak profilu'}</p>
+                    <input value={form.githubUsername ? form.githubUsername : 'Brak profilu'}></input>
                 </div>
                 <div className="phone-info">
                     <img src="/assets/phone.png" alt="Phone image"/>
-                    <p>{tel ? tel : 'Brak numeru'}</p>
+                    <input type='tel' value={form.tel ? form.tel : 'Brak numeru'} onChange={e => props.onChange('tel', e.target.value)}></input>
                 </div>
                 <div className="email-info">
                     <img src="/assets/envelope.png" alt="Envelope image"/>
-                    <p>{initialData.email}</p>
+                    <p>{form.initialData.email}</p>
                 </div>
             </div>
             <div className="student-personal-info-right-side">
                 <div className="about">
                     <p>O mnie</p>
-                    <p>{bio}</p>
+                    <textarea value={form.bio} onChange={e => props.onChange('bio', e.target.value)}></textarea>
                 </div>
                 <Btn text="Brak zainteresowania"></Btn>
                 <Btn text={checkStatus()}></Btn>
