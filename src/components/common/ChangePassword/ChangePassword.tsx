26deb8b33d8fd713a625d3470822464d11f7f71c
AnetaPy <aneta.pych15@gmail.com>
2023-12-12 10:44:04 +0100

    Added form and style to the ChangePassword

--- src/components/common/ChangePassword/ChangePassword.tsx
+++ src/components/common/ChangePassword/ChangePassword.tsx
@@ -1,7 +1,64 @@
+import {Btn} from "../Btn/Btn";
+import {FormEvent, useRef, useState} from "react";
+import './ChangePassword.css';
+
 export const ChangePassword = () => {
+    const [response, setResponse] = useState<boolean | null>(null);
+    const [form, setForm] = useState<string>({
+        oldPwd: '',
+        newPwd: '',
+        userId: ''
+    });
+    const text = useRef('');
+
+    const updateForm = async (key: string, value: any) => {
+        setForm(form => ({
+            ...form,
+            [key]: value
+        }))
+    };
+
+    const onSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+        const res = await fetch('http://localhost:3001/user/change-pass', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+            body: JSON.stringify(form)
+        });
+        const data = await res.json();
+        console.log(data)
+
+        if (res.status === 400 || res.status === 401 || res.status === 404 || res.status === 500) {
+            alert('Stare hasło jest błędne, wpisz poprawne.');
+            return;
+        } else {
+            setResponse(data.ok);
+        }
+    }
+
+    if (response) {
+        text.current = 'Poprawnie zmieniono hasło.'
+    }
+
     return (
-        <>
-        <h2>Zmiana hasła</h2>
-        </>
+        <div className="change-password">
+            <h2>Zmień hasło</h2>
+            <form onSubmit={onSubmit}>
+                <label>
+                    Stare hasło
+                    <input type="text" placeholder="Stare hasło" onChange={e => updateForm('oldPwd', e.target.value)}/>
+                </label>
+                <label>
+                    Nowe hasło
+                    <input type="text" placeholder="Nowe hasło" onChange={e => updateForm('newPwd', e.target.value)}/>
+                </label>
+
+                <Btn text="Zmień hasło"></Btn>
+            </form>
+            <p>{text.current}</p>
+        </div>
     )
 }
