bd9303f4ee97516b74ab284c660314d2f3f16aed
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-12-12 13:16:46 +0100

    add credentials to fetch

--- src/context/api/get-student-profile.ts
+++ src/context/api/get-student-profile.ts
@@ -1,5 +1,7 @@
 export const getStudentProfile = async (id: string) => {
-  const res = await fetch(`http://localhost:3001/student/${id}`);
+  const res = await fetch(`http://localhost:3001/student/${id}`, {
+    credentials: 'include',
+  });
 
   return await res.json();
 };
