0b040276edeee1492c254254708ea09584b92aa4
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-12-13 19:38:48 +0100

    implemented OneStudent

--- src/components/HRPanel/StudentsToInterview.tsx
+++ src/components/HRPanel/StudentsToInterview.tsx
@@ -1,7 +1,36 @@
+import { useState, useEffect } from 'react';
+import { StudentInitialInterface } from '../../../../megak-v3-headhunter-be-gr2/src/types/student';
+import { Spinner } from '../common/Spinner/Spinner';
+import { OneStudent } from './OneStudent';
+import { getReservedStudents } from 'src/api/get-reserved-students';
+import { Btn } from '../common/Btn/Btn';
+
 export const StudentsToInterview = () => {
-    return (
-        <>
-        <p>Do rozmowy komponent</p>
-        </>
-    )
-}
+  const [students, setStudents] = useState<StudentInitialInterface[] | null>(
+    null,
+  );
+
+  useEffect(() => {
+    (async () => {
+      const studentArray = await getReservedStudents();
+
+      setStudents(studentArray.students);
+    })();
+  }, []);
+
+  if (!students) return <Spinner />;
+
+  return (
+    <ul>
+      {students?.map(student => (
+        <li>
+          <OneStudent key={student.profile?.id} student={student}>
+            <Btn text="Pokaż CV"></Btn>
+            <Btn text="Brak Zainteresowania"></Btn>
+            <Btn text="Zatrudniony"></Btn>
+          </OneStudent>
+        </li>
+      ))}
+    </ul>
+  );
+};
