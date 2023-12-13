b2476ed1ce96b9ad1d120cb3bbc22229a5fe9687
AnetaPy <aneta.pych15@gmail.com>
2023-12-13 17:05:53 +0100

    Added information about the need to complete data on the home page

--- src/components/StudentPanel/StudentData.tsx
+++ src/components/StudentPanel/StudentData.tsx
@@ -2,11 +2,11 @@ import {StudentPersonalData} from "../common/StudentPersonalData/StudentPersonal
 import {StudentEmploymentExpectations} from "../common/StudentEducationAndExperience/StudentEmploymentExpectations/StudentEmploymentExpectations";
 import {StudentEducation} from "../common/StudentEducationAndExperience/StudentEducation/StudentEducation";
 import {StudentExperience} from "../common/StudentEducationAndExperience/StudentExperience/StudentExperience";
-import {StudentProfileInterface} from "types";
+import {StudentInitialInterface} from "types";
 import './StudentData.css';
 
 interface Props {
-    user: StudentProfileInterface
+    user: StudentInitialInterface
 }
 
 export const StudentData = (props: Props) => {
@@ -14,6 +14,7 @@ export const StudentData = (props: Props) => {
         <div className="student-data">
             <StudentPersonalData user={props.user}/>
             <div className="education-experience">
+                {props.user.profile ? '' : <h1>Uzupełnij dane na stronie głównej</h1>}
                 <StudentEmploymentExpectations user={props.user}/>
                 <StudentEducation user={props.user}/>
                 <StudentExperience user={props.user}/>
