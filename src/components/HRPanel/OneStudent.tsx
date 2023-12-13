2e2cb393a1c6fd4e00fe3b3d90917b9ed216dbe9
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-12-13 19:38:19 +0100

    changed OneStudent to make it reusable

--- src/components/HRPanel/OneStudent.tsx
+++ src/components/HRPanel/OneStudent.tsx
@@ -1,17 +1,17 @@
-import { FC, useState } from 'react';
+import { FC, ReactNode, useState } from 'react';
 import { StudentInitialInterface } from 'types';
-import { Btn } from '../common/Btn/Btn';
 import { LiaAngleDownSolid } from 'react-icons/lia';
 
 import './OneStudent.css';
 import { OneStudentProfile } from './OneStudentProfile';
-import { reserveStudentByHr } from 'src/api/reserve-student-by-hr';
 
 interface Props {
   student: StudentInitialInterface;
+  isReserved?: boolean;
+  children: ReactNode;
 }
 
-export const OneStudent: FC<Props> = ({ student }) => {
+export const OneStudent: FC<Props> = ({ student, isReserved, children }) => {
   const [isProfileVisible, setIsProfileVisible] = useState(false);
   const [rotationAngle, setRotationAngle] = useState(0);
 
@@ -20,25 +20,17 @@ export const OneStudent: FC<Props> = ({ student }) => {
     setRotationAngle(rotationAngle + 180);
   };
 
-  const reserveStudent = async (email: string) => {
-    const student = await reserveStudentByHr(email);
-    console.log(student.reservationExpirationDate);
-    return {
-      expirationDate: student.reservationExpirationDate,
-    };
-  };
-
   return (
     <>
       <div className="one-student">
         <div className="student-name">
-          {student.profile?.firstName} {student.profile?.lastName.charAt(0)}.
+          {student.profile?.firstName}{' '}
+          {isReserved
+            ? student.profile?.lastName.charAt(0) + '.'
+            : student.profile?.lastName}
         </div>
-        <Btn
-          text="Zarezerwuj rozmowę"
-          onClick={() => reserveStudent(student.email)}
-        />
 
+        {children}
         <LiaAngleDownSolid
           className="arrow"
           style={{ transform: `rotate(${rotationAngle}deg)` }}
