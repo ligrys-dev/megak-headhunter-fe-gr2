f6fb886819f8bf4e4cafce108f36a9a8b6203fe7
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-12-13 14:32:28 +0100

    implemented view for students

--- src/components/HRPanel/AvailableStudents.tsx
+++ src/components/HRPanel/AvailableStudents.tsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from 'react';
+import { StudentInitialInterface } from 'types';
+import { getStudentsForRecruiter } from 'src/api/get-students-for-recruiter';
+import { OneStudent } from './OneStudent';
+import { Spinner } from '../common/Spinner/Spinner';
+
 export const AvailableStudents = () => {
-    return (
-        <>
-        <p>Dostępni kursanci komponet</p>
-        </>
-    )
-}
+  const [students, setStudents] = useState<StudentInitialInterface[] | null>(
+    null,
+  );
+
+  useEffect(() => {
+    (async () => {
+      const studentArray = await getStudentsForRecruiter();
+
+      setStudents(studentArray.students);
+    })();
+  }, []);
+
+  if (!students) return <Spinner />;
+
+  return (
+    <ul>
+      {students?.map(student => (
+        <li>
+          <OneStudent key={student.profile?.id} student={student} />
+        </li>
+      ))}
+    </ul>
+  );
+};
