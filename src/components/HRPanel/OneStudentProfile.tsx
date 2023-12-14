d3174269636ae833e9584e73fc6f4cea44b48e95
AnetaPy <aneta.pych15@gmail.com>
2023-12-14 13:17:36 +0100

    Added mobile version to the OneStudentProfile

--- src/components/HRPanel/OneStudentProfile.tsx
+++ src/components/HRPanel/OneStudentProfile.tsx
@@ -1,105 +1,138 @@
-import { FC } from 'react';
-import { ContractType, StudentInitialInterface, TypeWork } from 'types';
+import {FC} from 'react';
+import {ContractType, StudentInitialInterface, TypeWork} from 'types';
 
 import './OneStudentProfile.css';
 
 interface Props {
-  student: StudentInitialInterface;
+    student: StudentInitialInterface;
 }
 
 const getExpectedTypeWorkString = (expectedTypeWork: TypeWork) => {
-  switch (expectedTypeWork) {
-    case 0:
-      return 'Na miejscu';
-    case 1:
-      return 'Gotowość do przeprowadzki';
-    case 2:
-      return 'Zdalnie';
-    case 3:
-      return 'Hybrid';
-    case 4:
-      return 'Nie ma znaczenia';
-    default:
-      return 'Nieokreślone';
-  }
+    switch (expectedTypeWork) {
+        case 0:
+            return 'Na miejscu';
+        case 1:
+            return 'Gotowość do przeprowadzki';
+        case 2:
+            return 'Zdalnie';
+        case 3:
+            return 'Hybrid';
+        case 4:
+            return 'Nie ma znaczenia';
+        default:
+            return 'Nieokreślone';
+    }
 };
 
 const getExpectedContractType = (exprectedContractType: ContractType) => {
-  switch (exprectedContractType) {
-    case 0:
-      return 'Umowa zlecenie';
-    case 1:
-      return 'B2B';
-    case 2:
-      return 'Umowa o pracę';
-    case 3:
-      return 'Nie ma znaczenia';
-    default:
-      return 'Nieokreślone';
-  }
+    switch (exprectedContractType) {
+        case 0:
+            return 'Umowa zlecenie';
+        case 1:
+            return 'B2B';
+        case 2:
+            return 'Umowa o pracę';
+        case 3:
+            return 'Nie ma znaczenia';
+        default:
+            return 'Nieokreślone';
+    }
 };
 
-export const OneStudentProfile: FC<Props> = ({ student }) => (
-  <ul className="one-student-profile">
-    <li>
-      <div className="profile-property">Ocena przejścia kursu:</div>
-      <div className="profile-property-value">{student.courseCompletion}</div>
-    </li>
-    <li>
-      <div className="profile-property">
-        Ocena aktywności zaangażowania na kursie
-      </div>
-      <div className="profile-property-value">{student.courseEngagement}</div>
-    </li>
+export const OneStudentProfile: FC<Props> = ({student}) => {
+    const checkMonthsOfCommercialExp = () => {
+        const arrayOfMonths = [2, 3, 4];
+        if (student.profile?.monthsOfCommercialExp === 1) {
+            return '1 miesiąc'
+        } else if (arrayOfMonths.includes(student.profile?.monthsOfCommercialExp)) {
+            return `${student.profile?.monthsOfCommercialExp} miesiące`
+        } else {
+            return `${student.profile?.monthsOfCommercialExp} miesięcy`
+        }
+    }
 
-    <li>
-      <div className="profile-property">Ocena pracy w zespole Scrum</div>
-      <div className="profile-property-value">{student.teamProjectDegree}</div>
-    </li>
-    <li>
-      <div className="profile-property">Preferowane miejsce pracy</div>
-      <div className="profile-property-value">
-        {getExpectedTypeWorkString(student.profile?.expectedTypeWork)}
-      </div>
-    </li>
-    <li>
-      <div className="profile-property">
-        Docelowe miasto, gdzie chce pracować kandydat
-      </div>
-      <div className="profile-property-value">
-        {student.profile?.targetWorkCity}
-      </div>
-    </li>
-    <li>
-      <div className="profile-property">Oczekiwany typ kontraktu</div>
-      <div className="profile-property-value">
-        {getExpectedContractType(student.profile?.expectedContractType)}
-      </div>
-    </li>
-    <li>
-      <div className="profile-property">
-        {' '}
-        Oczekiwane wynagrodzenie miesięczne netto
-      </div>
-      <div className="profile-property-value">
-        {student.profile?.expectedSalary}
-      </div>
-    </li>
-    <li>
-      <div className="profile-property">
-        Zgoda na odbycie bezpłatnych praktyk/stażu na początek
-      </div>
-      <div className="profile-property-value">
-        {student.profile?.canTakeApprenticeship ? 'Tak' : 'Nie'}
-      </div>
-    </li>
-    <li>
-      <div className="profile-property">
-        Komercyjne doświadczenie w programowaniu
-      </div>
-      <div className="profile-property-value">
-        {student.profile?.monthsOfCommercialExp}
-      </div>
-    </li>
-  </ul>
-);
+    return (
+        <ul className="one-student-profile">
+            <div className="left-side">
+                <li>
+                    <div className="profile-property">
+                        <p>Ocena przejścia kursu:</p>
+                    </div>
+                    <div className="profile-property-value">
+                        <p>{Number(student.courseCompletion).toFixed(0)} / 5</p></div>
+                </li>
+                <li>
+                    <div className="profile-property">
+                        <p>Ocena aktywności zaangażowania na kursie</p>
+                    </div>
+                    <div className="profile-property-value">
+                        <p>{Number(student.courseEngagement).toFixed(0)} / 5</p></div>
+                </li>
+                <li>
+                    <div className="profile-property">
+                        <p>Ocena kodu w projekcie własnym</p>
+                    </div>
+                    <div className="profile-property-value">
+                        <p>{Number(student.projectDegree).toFixed(0)} / 5</p></div>
+                </li>
+                <li>
+                    <div className="profile-property">
+                        <p>Ocena pracy w zespole Scrum</p>
+                    </div>
+                    <div className="profile-property-value">
+                        <p>{Number(student.teamProjectDegree).toFixed(0)} / 5</p></div>
+                </li>
+                <li>
+                    <div className="profile-property">
+                        <p>Preferowane miejsce pracy</p>
+                    </div>
+                    <div className="profile-property-value">
+                        <p>{getExpectedTypeWorkString(student.profile?.expectedTypeWork)}</p>
+                    </div>
+                </li>
+            </div>
+            <div className="right-side">
+                <li>
+                    <div className="profile-property">
+                        <p>Docelowe miasto, gdzie chce pracować kandydat</p>
+                    </div>
+                    <div className="profile-property-value">
+                        <p>{student.profile?.targetWorkCity}</p>
+                    </div>
+                </li>
+                <li>
+                    <div className="profile-property">
+                        <p>Oczekiwany typ kontraktu</p>
+                    </div>
+                    <div className="profile-property-value">
+                        <p>{getExpectedContractType(student.profile?.expectedContractType)}</p>
+                    </div>
+                </li>
+                <li>
+                    <div className="profile-property">
+                        <p>Oczekiwane wynagrodzenie miesięczne netto</p>
+                    </div>
+                    <div className="profile-property-value">
+                        <p>{Number(student.profile?.expectedSalary).toFixed(0) + ' zł'}</p>
+                    </div>
+                </li>
+                <li>
+                    <div className="profile-property">
+                        <p>Zgoda na odbycie bezpłatnych praktyk/stażu na początek</p>
+                    </div>
+                    <div className="profile-property-value">
+                        <p>{student.profile?.canTakeApprenticeship ? 'Tak' : 'Nie'}</p>
+                    </div>
+                </li>
+                <li>
+                    <div className="profile-property">
+                        <p>Komercyjne doświadczenie w programowaniu</p>
+                    </div>
+                    <div className="profile-property-value">
+                        <p>{student.profile ? checkMonthsOfCommercialExp() : ''}</p>
+                    </div>
+                </li>
+            </div>
+        </ul>
+    );
+}
