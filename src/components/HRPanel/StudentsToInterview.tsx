4c9f088a13b543a7361074a32029fa102ba1e2e1
AnetaPy <aneta.pych15@gmail.com>
2023-12-14 20:05:07 +0100

    Update CSS file

--- src/components/HRPanel/StudentsToInterview.tsx
+++ src/components/HRPanel/StudentsToInterview.tsx
@@ -1,49 +1,52 @@
-import { useState, useEffect } from 'react';
-import { StudentInitialInterface } from '../../../../megak-v3-headhunter-be-gr2/src/types/student';
-import { Spinner } from '../common/Spinner/Spinner';
-import { OneStudent } from './OneStudent';
-import { getReservedStudents } from 'src/api/get-reserved-students';
-import { Btn } from '../common/Btn/Btn';
+import {useState, useEffect} from 'react';
+import {StudentInitialInterface} from 'types';
+import {Spinner} from '../common/Spinner/Spinner';
+import {OneStudent} from './OneStudent';
+import {getReservedStudents} from 'src/api/get-reserved-students';
+import {Btn} from '../common/Btn/Btn';
+import './StudentsToInterview.css';
 
 export const StudentsToInterview = () => {
-  const [students, setStudents] = useState<StudentInitialInterface[] | null>(
-    null,
-  );
+    const [students, setStudents] = useState<StudentInitialInterface[] | null>(
+        null,
+    );
 
-  useEffect(() => {
-    (async () => {
-      const studentArray = await getReservedStudents();
+    useEffect(() => {
+        (async () => {
+            const studentArray = await getReservedStudents();
 
-      setStudents(studentArray.students);
-    })();
-  }, []);
+            setStudents(studentArray.students);
+        })();
+    }, []);
 
-  const showCv = () => {
-    console.log('show cv');
-  };
-  const handleNotInterested = () => {
-    console.log('not interested');
-  };
-  const handleHire = () => {
-    console.log('hired');
-  };
+    const showCv = () => {
+        console.log('show cv');
+    };
+    const handleNotInterested = () => {
+        console.log('not interested');
+    };
+    const handleHire = () => {
+        console.log('hired');
+    };
 
-  if (!students) return <Spinner />;
+    if (!students) return <Spinner/>;
 
-  return (
-    <ul>
-      {students?.map(student => (
-        <li>
-          <OneStudent key={student.profile?.id} student={student} isReserved>
-            <Btn text="Pokaż CV" onClick={() => showCv()}></Btn>
-            <Btn
-              text="Brak Zainteresowania"
-              onClick={() => handleNotInterested()}
-            ></Btn>
-            <Btn text="Zatrudniony" onClick={() => handleHire()}></Btn>
-          </OneStudent>
-        </li>
-      ))}
-    </ul>
-  );
+    return (
+        <div className="students-to-interview">
+            <ul>
+                {students?.map(student => (
+                    <li key={student.profile?.id}>
+                        <OneStudent key={student.profile?.id} student={student} isReserved>
+                            <Btn text="Pokaż CV" onClick={() => showCv()}></Btn>
+                            <Btn
+                                text="Brak Zainteresowania"
+                                onClick={() => handleNotInterested()}
+                            ></Btn>
+                            <Btn text="Zatrudniony" onClick={() => handleHire()}></Btn>
+                        </OneStudent>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
