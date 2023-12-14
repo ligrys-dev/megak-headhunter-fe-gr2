1618d6bc6fee586478c9c6416bf48502b1d37902
AnetaPy <aneta.pych15@gmail.com>
2023-12-14 14:27:49 +0100

    Created structure and style for FilterPanel

--- src/components/common/FilterPanel/FilterPanel.tsx
+++ src/components/common/FilterPanel/FilterPanel.tsx
@@ -2,7 +2,15 @@ import './FilterPanel.css';
 
 export const FilterPanel = () => {
     return (
-        <>
-        <p>Ok</p></>
+        <div className="filter-panel">
+            <div className="search">
+                <img src="/assets/search.svg" alt="Magnifier"/>
+                <input type="text" placeholder="Szukaj"/>
+            </div>
+            <button>
+                <img src="/assets/arrow.png" alt="Arrow"/>
+                <p>Filtrowanie</p>
+            </button>
+        </div>
     )
 }
