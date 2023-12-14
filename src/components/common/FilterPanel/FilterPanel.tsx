07b12ec5a864a76b90e1c3980b6122470fa4dfcf
AnetaPy <aneta.pych15@gmail.com>
2023-12-14 17:43:00 +0100

    Added real-time filtering

--- src/components/common/FilterPanel/FilterPanel.tsx
+++ src/components/common/FilterPanel/FilterPanel.tsx
@@ -1,12 +1,41 @@
 import './FilterPanel.css';
 import {FilterPopup} from "../FilterPopup/FilterPopup";
+import {useEffect, useState} from "react";
+import {getStudentsForRecruiter} from "../../../api/get-students-for-recruiter";
 
-export const FilterPanel = () => {
+interface Props {
+    onChildClick: () => {};
+}
+
+export const FilterPanel = (props: Props) => {
+    const [searchItem, setSearchItem] = useState('');
+    const [students, setStudents] = useState([]);
+    useEffect(() => {
+        (async () => {
+            const studentArray = await getStudentsForRecruiter();
+            setStudents(studentArray.students);
+        })();
+    }, []);
+
+    const handleInputChange = (e) => {
+        const searchTerm = e.target.value;
+        setSearchItem(searchTerm)
+
+        const filteredItems = students.filter((user) =>
+            user.profile.firstName.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        props.onChildClick(filteredItems)
+    }
+
     return (
         <div className="filter-panel">
             <div className="search">
                 <img src="/assets/search.svg" alt="Magnifier"/>
-                <input type="text" placeholder="Szukaj"/>
+                <input
+                    type="text"
+                    value={searchItem}
+                    onChange={handleInputChange}
+                    placeholder="Szukaj"/>
             </div>
             <FilterPopup></FilterPopup>
         </div>
