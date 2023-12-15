276cd3fe0c2bce3f3674d94c2b9169574b37d338
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-12-15 17:03:04 +0100

    Revert "Merge pull request #58 from ligrys-dev/feature/filtration"
    This reverts commit c8e8d81ff53c95cacf553a5e336eeca38ed66f0c, reversing
    changes made to 63092faee9c8ad52a06d0f72fa301514ccef49c8.

--- src/api/get-student.ts
+++
@@ -1,9 +0,0 @@
-import { StudentInitialInterface, UserType } from 'types';
-
-export const getStudent = async (): Promise<StudentInitialInterface | null> => {
-  const res = await fetch('http://localhost:3001/user', {
-    credentials: 'include',
-  });
-  const data: UserType = await res.json();
-  return data.student;
-};
